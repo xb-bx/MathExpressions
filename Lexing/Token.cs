b3cff7f49eb15e34463e83e87d80e963844965f0
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-03-29 11:23:34 +0300

    Added Token class

---
+++ Lexing/Token.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathEpxressions.Lexing
+{
+    public class Token
+    {
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public TokenType Type { get; private set; }
+        public string Value { get; private set; }
+
+        
+
+    }
+}
