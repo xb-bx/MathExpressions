b676025934bdfdf36f06b45631fdb51d89168ab0
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-03-31 18:48:29 +0300

    Added ConstantExpression class

---
+++ Parsing/AST/ConstantExpression.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+namespace MathEpxressions.Parsing.AST
+{
+    public class ConstantExpression : IExpression
+    {
+        public double Constant { get; private set; }
+
+        public ConstantExpression(double constant)
+        {
+            Constant = constant;
+        } 
+ 
+        public override string ToString()
+        {
+            return Constant.ToString();
+        }
+    }
+}
