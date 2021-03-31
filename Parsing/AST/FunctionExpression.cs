cbe0618c8fbdda0d69d1a2f67e1a7d5e3e551cf1
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-03-31 18:50:13 +0300

    Added FunctionExpression class

---
+++ Parsing/AST/FunctionExpression.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathEpxressions.Parsing.AST
+{
+    public class FunctionExpression : IExpression
+    {
+        public FunctionExpression(string name, List<IExpression> args)
+        {
+            Name = name;
+            Args = args;
+        }
+
+        public string Name { get; set; }
+        public List<IExpression> Args { get; set; } 
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append('(');
+            for(int i = 0; i < Args.Count; i++)
+            {
+                sb.Append(Args[i].ToString());
+                if(i<Args.Count - 1){
+                    sb.Append(',');
+                }
+            }    
+            sb.Append(')');
+            return sb.ToString();
+        }
+    }
+}
