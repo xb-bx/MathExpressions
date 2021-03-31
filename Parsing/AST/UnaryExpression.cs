7d1d09b3aa43cbaac2155d6cef2fe38f02e25ad4
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-03-31 18:49:20 +0300

    Added UnaryExpression class

---
+++ Parsing/AST/UnaryExpression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+namespace MathEpxressions.Parsing.AST
+{
+    public class UnaryExpression : IExpression
+    {
+        public UnaryExpression(IExpression expression, char @operator)
+        {
+            Expression = expression;
+            Operator = @operator;
+        }
+
+        public IExpression Expression { get; private set; }
+        public char Operator { get; private set; }
+        
+        
+         
+        public override string ToString()
+        {
+            return $"{Operator}{Expression}";
+        }
+    }
+}
