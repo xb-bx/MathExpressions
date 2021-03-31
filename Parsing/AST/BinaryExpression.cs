66365ffe86ccebdb7cbff38233644547b1856a1d
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-03-31 18:49:37 +0300

    Added BinaryExpression class

---
+++ Parsing/AST/BinaryExpression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+namespace MathEpxressions.Parsing.AST
+{
+    public class BinaryExpression : IExpression
+    {
+        public BinaryExpression(IExpression firstExpression, IExpression secondExpression, char @operator)
+        {
+            FirstExpression = firstExpression;
+            SecondExpression = secondExpression;
+            Operator = @operator;
+        }
+
+        public IExpression FirstExpression { get; private set; }
+        public IExpression SecondExpression { get; private set; }
+        public char Operator { get; set; } 
+        public override string ToString()
+        {
+            return $"({FirstExpression} {Operator} {SecondExpression})";
+        }
+    }
+}
