ee6823cd454e0a182429af0e940c9ffdca5019e6
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-04-07 16:09:52 +0300

    Added AddDefaultFunctions method to EvaluationEngine

--- src/MathExpressions/EvaluationEngine.cs
+++ src/MathExpressions/EvaluationEngine.cs
@@ -22,8 +22,38 @@ namespace MathExpressions
         public EvaluationEngine(CultureInfo cultureInfo = null)
         {
             this.cultureInfo = cultureInfo;
-            parser = new(cultureInfo); 
+            parser = new(cultureInfo);
+            
+        }
+        public void AddDefaultFunctions()
+        {
+            functions["sin"] = (Func<double, double>)(x => Math.Sin(x));
+            functions["sinh"] = (Func<double, double>)(x => Math.Sinh(x));
+            functions["asin"] = (Func<double, double>)(x => Math.Asin(x));
+            functions["asinh"] = (Func<double, double>)(x => Math.Asinh(x));
+            functions["cos"] = (Func<double, double>)(x => Math.Cos(x));
+            functions["acos"] = (Func<double, double>)(x => Math.Acos(x));
+            functions["acosh"] = (Func<double, double>)(x => Math.Acosh(x));
+            functions["cosh"] = (Func<double, double>)(x => Math.Cosh(x));
+            functions["tan"] = (Func<double, double>)(x => Math.Tan(x));
+            functions["atan"] = (Func<double, double>)(x => Math.Atan(x));
+            functions["atanh"] = (Func<double, double>)(x => Math.Atanh(x));
+            functions["tanh"] = (Func<double, double>)(x => Math.Tanh(x));
+            functions["ctg"] = (Func<double, double>)(x => 1.0 / Math.Tan(x));
+            functions["floor"] = (Func<double, double>)(x => Math.Floor(x));
+            functions["ceiling"] = (Func<double, double>)(x => Math.Ceiling(x));
+            functions["round"] = (Func<double, double>)(x => Math.Round(x));
+            functions["min"] = (Func<double, double, double>)((x, y) => Math.Min(x, y));
+            functions["max"] = (Func<double, double, double>)((x, y) => Math.Max(x, y));
+            functions["clamp"] = (Func<double, double, double, double>)((x, y, z) => Math.Clamp(x, y, z));
+            functions["sqrt"] = (Func<double, double>)((x) => Math.Sqrt(x));
+            functions["cbrt"] = (Func<double, double>)((x) => Math.Cbrt(x));
+            functions["log"] = (Func<double, double>)((x) => Math.Log(x));
+            functions["abs"] = (Func<double, double>)(x => Math.Abs(x));
+            functions["rad"] = (Func<double, double>)(x => x * Math.PI / 180);
+            functions["deg"] = (Func<double, double>)(x => x * 180 / Math.PI);
 
+            compiler.AddManyFunctions(functions.AsEnumerable().Select(x => (x.Key, x.Value.Method)));
         }
         public Delegate this[string name]
         {
@@ -38,6 +68,7 @@ namespace MathExpressions
                 {
                     functions.Add(name, value);
                 }
+                compiler.AddFunction(name, value);
             }
         }
         private double EvaluateExpression(IExpression expression, Dictionary<string, double> variables)
