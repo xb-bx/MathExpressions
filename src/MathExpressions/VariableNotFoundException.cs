78ff1d5a1a6a0f3e99f7b46e74b9ca8693b58858
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-04-07 15:23:41 +0300

    Added VariableNotFoundException class

---
+++ src/MathExpressions/VariableNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathExpressions
+{
+    public class VariableNotFoundException : Exception
+    {
+        public string Name { get; private set; }
+        public VariableNotFoundException(string name) : base($"Variable {name} not found")
+        {
+            Name = name;
+        }
+    }
+}
