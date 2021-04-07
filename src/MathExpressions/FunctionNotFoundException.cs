910c31a69b69583b82a4a4343fbed3b486b092a2
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-04-07 15:24:09 +0300

    Added FunctionNotFoundException class

---
+++ src/MathExpressions/FunctionNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathExpressions
+{
+    public class FunctionNotFoundException : Exception
+    {
+        public string Name { get; private set; }
+        public FunctionNotFoundException(string name) : base($"Function {name} not found")
+        {
+            Name = name;
+        }
+    }
+}
